5995dddfc116980bf918f1100430ace59e465a68
Deadburn <cihanalicii7@gmail.com>
2023-08-16 17:03:29 +0300

    contact us fix

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -2,58 +2,60 @@ import React from 'react'
 import './ContactUs.css'
 
 export default function ContactUs() {
-  return (
-    <div className='mainDiv'>
-        <div className='leftColumn'>
-            <div className='headerDiv'>
-                <span className='h1'>To Contact Us</span>
-                <div className='formBlock'>
-                    <div>
-                        <span className='callBackText'>We will call you back</span>
-                    </div>
-                    <div className='textArea'>
-                        <input className='textNumber' placeholder='+90 xxx xxx xx xx'></input>
-                        <button className='phoneBtn'>BOOK A CALL</button>
-                    </div>
-                </div>
-            </div>
-            <div className='adressArea'>
-                <div className='phoneAdress'>
-                    <div className='h2Div'>
-                        <span className='h2'>Phone</span>
-                    </div>
-                    <div className='phoneNumbers'>
-                        <div>
-                            <img src='/images/call.png'></img>
-                            <span>+380980099777</span>
-                        </div>
-                        <div>
-                            <img src='/images/call.png'></img>
-                            <span>+380980099777</span>
+    return (
+        <>
+            <div className='contactUsMainDiv'>
+                <div className='contactUsLeftCol'>
+                    <div className='headerDiv'>
+                        <span className='h1'>To Contact Us</span>
+                        <div className='formBlock'>
+                            <div>
+                                <span className='callBackText'>We will call you back</span>
+                            </div>
+                            <div className='textArea'>
+                                <input className='textNumber' placeholder='+90 xxx xxx xx xx'></input>
+                                <button className='phoneBtn'>BOOK A CALL</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='adress'>
-                    <div className='h2Div'>
-                        <span className='h2'>Adress</span>
-                    </div>
-                    <div className='adressLocation'>
-                    <div>
-                            <span>+opening hours: 8 to 11 p.m.</span>
+                    <div className='adressArea'>
+                        <div className='phoneAdress'>
+                            <div className='h2Div'>
+                                <span className='h2'>Phone</span>
+                            </div>
+                            <div className='phoneNumbers'>
+                                <div>
+                                    <img src='/images/call.png' alt='right' />
+                                    <span>+380980099777</span>
+                                </div>
+                                <div>
+                                    <img src='/images/call.png' alt='right' />
+                                    <span>+380980099777</span>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <img src='/images/pindrop.png'></img>
-                            <span>+opening hours: 8 to 11 p.m.</span>
-                        </div>
+                        <div className='adress'>
+                            <div className='h2Div'>
+                                <span className='h2'>Adress</span>
+                            </div>
+                            <div className='adressLocation'>
+                                <div>
+                                    <span>+opening hours: 8 to 11 p.m.</span>
+                                </div>
+                                <div>
+                                    <img src='/images/pindrop.png' alt='right' />
+                                    <span>+opening hours: 8 to 11 p.m.</span>
+                                </div>
 
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className='rightColumn'>
-            <img className='rightColumn' src='/images/rightColumn.png'></img>
-        </div>
+                <div className='rightColumn'>
+                    <img className='rightColumn' src='/images/rightColumn.png' alt='right' />
+                </div>
 
-    </div>
-  )
+            </div>
+        </>
+    )
 }
--- src/Components/ContentCategory.js
+++ src/Components/ContentCategory.js
@@ -1,77 +1,142 @@
 import React from 'react'
 import './ContentCategory.css'
 
-      const LeftCards=[
-        {
-          category: "Fresh Flovers",
-        },
-        {
-          img:"./images/image2.png",
-        },
-        {
-          category: "Live Plants",
-        },
-        {
-          img:"./images/image4.png",
-        },
-        {
-          category: "Fresheners",
-        }
+const LeftCards = [
+  {
+    category: "Fresh Flovers",
+    img: true
+  },
 
-      ]
+  {
+    category: "Live Plants",
+    img: true
+  },
+  {
+    category: "Fresheners",
+    img: false
+  }
 
-      const RightCards = [
-        {
-          img: "./images/image1.png",
-        },
-        {
-          category: "Dried Flowers",
-        },
-        {
-          img:"./images/image3.png",
-        },
-        {
-          category: "Aroma Candels",
-        },
-        {
-          img:"./images/image5.png",
-        },
-      ]
+]
+
+const RightCards = [
+  {
+    category: "Dried Flowers",
+  },
+  {
+    category: "Aroma Candels",
+  },
+]
 
 
-    const ContentCategory = () => {
-  
-      
-        return (
-          <div className='rightColumn'>
+const ContentCategory = () => {
 
-            <div className='leftCard'>
-            {LeftCards.map((i,key) => (
-              <div key={key} >
-                <span className='imgName'>{i.category}</span>
-                <img className='img' src={i.img}></img>
+
+  return (
+    <>
+      <div>
+        {LeftCards?.map((item, key) => (
+          <div key={key}>
+            <div className='cardCategory'>
+              <div className='cardCategoryLabel'>
+                <span>{item?.category}</span>
               </div>
-            ))}
+              <div className='categoryText'>
+                <span className='categoryTextBtn'>Shop Now</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M15.1397 6L13.6815 7.50351L17 11H3V13H17L13.702 16.5035L15.167 18L21 11.986L15.1397 6Z" fill="black" />
+                </svg>
+              </div>
             </div>
 
-                 <div className='lightCard'>
-            {RightCards.map((i,key) => (
-              <div key={key}>
-                <span className='imgName'>{i.category}</span>
-                <img className='img' src={i.img}></img>
+            {item?.img && <div className={`cardItemImg${2 * (key + 1)}`} />}
+          </div>
+        ))}
+
+
+        {/* <div className='cardCategory'>
+          <div className='cardCategoryLabel'>
+            <span>Live Plants</span>
+          </div>
+          <div className='categoryText'>
+            <span className='categoryTextBtn'>Shop Now</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M15.1397 6L13.6815 7.50351L17 11H3V13H17L13.702 16.5035L15.167 18L21 11.986L15.1397 6Z" fill="black" />
+            </svg>
+          </div>
+        </div>
+
+        <div className='cardItemImg1' />
+
+
+        <div className='cardCategory'>
+          <div className='cardCategoryLabel'>
+            <span>Fresheners</span>
+          </div>
+          <div className='categoryText'>
+            <span className='categoryTextBtn'>Shop Now</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M15.1397 6L13.6815 7.50351L17 11H3V13H17L13.702 16.5035L15.167 18L21 11.986L15.1397 6Z" fill="black" />
+            </svg>
+          </div>
+        </div> */}
+      </div>
+
+
+      <div>
+        {RightCards?.map((item, key) => (
+          <div key={key}>
+            <div className={`cardItemImg${2 * (key + 1) - 1}`} />
+
+            <div className='cardCategory'>
+              <div className='cardCategoryLabel'>
+                <span>{item?.category}</span>
               </div>
-              
-            ))}
-          
+              <div className='categoryText'>
+                <span className='categoryTextBtn'>Shop Now</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M15.1397 6L13.6815 7.50351L17 11H3V13H17L13.702 16.5035L15.167 18L21 11.986L15.1397 6Z" fill="black" />
+                </svg>
+              </div>
+            </div>
+            
+            {key === 1 && <div className={`cardItemImg5`} />}
           </div>
-          
+        ))}
+        {/* <div className='cardItemImg3' />
+
+        <div className='cardCategory'>
+          <div className='cardCategoryLabel'>
+            <span>Fresh Flowers</span>
           </div>
-          
-          
-          
-          
-        );
-      };
+          <div className='categoryText'>
+            <span className='categoryTextBtn'>Shop Now</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M15.1397 6L13.6815 7.50351L17 11H3V13H17L13.702 16.5035L15.167 18L21 11.986L15.1397 6Z" fill="black" />
+            </svg>
+          </div>
+        </div>
+
+        <div className='cardItemImg4' />
+
+        <div className='cardCategory'>
+          <div className='cardCategoryLabel'>
+            <span>Live Plants</span>
+          </div>
+          <div className='categoryText'>
+            <span className='categoryTextBtn'>Shop Now</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M15.1397 6L13.6815 7.50351L17 11H3V13H17L13.702 16.5035L15.167 18L21 11.986L15.1397 6Z" fill="black" />
+            </svg>
+          </div>
+        </div>
+
+        <div className='cardItemImg5' /> */}
+
+      </div>
+
+    </>
+  );
+};
 
 
 export default ContentCategory
